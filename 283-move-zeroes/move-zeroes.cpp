5465a963b7850e2df5de19edef5029d6f93e71dc
Sadab Ansari <145316401+Sadab-Ansari@users.noreply.github.com>
2026-07-15 22:02:55 +0530

    Time: 4 ms (10.71%) | Memory: 23.9 MB (19.31%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int write=0;
+        for (int i =0; i<nums.size(); i++){
+            if (nums[i]!=0){
+                nums[write]=nums[i];
+                write++;
+            }
+        }
+        while(write<nums.size()){
+            nums[write]=0;
+            write++;
+        }
+    }
+};
