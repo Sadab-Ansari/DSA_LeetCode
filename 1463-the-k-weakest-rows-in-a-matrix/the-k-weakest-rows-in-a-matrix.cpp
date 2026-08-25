e534b51a3cf8ac4051ad6aa66b7e461f31d65230
Sadab Ansari <145316401+Sadab-Ansari@users.noreply.github.com>
2026-08-25 23:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (68.20%) - LeetSync

---
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int, int>> rows;
+
+        for (int i = 0; i < mat.size(); i++) {
+            int soldiers = 0;
+
+            for (int j = 0; j < mat[i].size(); j++) {
+                soldiers += mat[i][j];
+            }
+
+            rows.push_back({soldiers, i});
+        }
+
+        sort(rows.begin(), rows.end());
+
+        vector<int> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(rows[i].second);
+        }
+
+        return ans;
+    }
+};
