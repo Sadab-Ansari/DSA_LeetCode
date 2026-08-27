cb6d55baf9dac5cf3364ad51b68c2fe411f50bd1
Sadab Ansari <145316401+Sadab-Ansari@users.noreply.github.com>
2026-08-27 17:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (71.17%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+
+        vector<bool> visited(n, false);
+        queue<int> q;
+
+        q.push(0);
+        visited[0] = true;
+
+        int count = 0;
+
+        while (!q.empty()) {
+            int room = q.front();
+            q.pop();
+
+            count++;
+
+            for (int key : rooms[room]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    q.push(key);
+                }
+            }
+        }
+
+        return count == n;
+    }
+};
