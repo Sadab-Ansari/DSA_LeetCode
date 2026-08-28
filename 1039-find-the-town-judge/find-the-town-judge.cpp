d5152a64b9dfb065110e30aaf85d5eb0381f1566
Sadab Ansari <145316401+Sadab-Ansari@users.noreply.github.com>
2026-08-28 18:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 64.9 MB (95.96%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> score(n + 1, 0);
+
+        for (auto &t : trust) {
+            int a = t[0];
+            int b = t[1];
+
+            score[a]--; // a trusts someone, so can't be judge
+            score[b]++; // b is trusted by someone
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
